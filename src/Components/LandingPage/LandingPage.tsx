5a10930f0f5cf1e05eb3a14377a9913edf018c9f
Anon <opss59332@gmail.com>
2023-09-03 22:58:05 +0530

    Landing page responsiveness fixed

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
   return (
     <>
       <section className="bg-[#ccc5b9] w-screen h-screen font-gloock flex items-center justify-center flex-col">
-        <h1 className="text-[#252422] text-[20em] md:text-[5em] lg:text-[10em]">YOUDIO</h1>
+        <h1 className="text-[#252422] text-[20em]  lg:text-[5.5em]">YOUDIO</h1>
         <span className="font-golos text-black font-semibold mb-10 md:mb-8 md:text-xs">
           YouTube to Audio is here with 0 downloads.
         </span>
